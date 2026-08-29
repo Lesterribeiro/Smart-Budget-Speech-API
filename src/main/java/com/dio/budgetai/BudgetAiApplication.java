2c7f3fa3989444eaaade54c1d678fa840ba8198e
Lesterribeiro <142675012+Lesterribeiro@users.noreply.github.com>
2026-08-29 17:31:09 -0300

    Add main application class for BudgetAi

--- src/main/java/com/dio/budgetai/BudgetAiApplication.java
+++ src/main/java/com/dio/budgetai/BudgetAiApplication.java
@@ -1,1 +1,12 @@
+package com.dio.budgetai;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAiApplication.class, args);
+    }
+}
