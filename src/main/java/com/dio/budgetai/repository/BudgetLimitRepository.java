85dd1c763c86c89920588f4ef3c3179761526b95
Lesterribeiro <142675012+Lesterribeiro@users.noreply.github.com>
2026-08-29 17:32:17 -0300

    Add method to find BudgetLimit by category

--- src/main/java/com/dio/budgetai/repository/BudgetLimitRepository.java
+++ src/main/java/com/dio/budgetai/repository/BudgetLimitRepository.java
@@ -1,1 +1,9 @@
+package com.dio.budgetai.repository;
 
+import com.dio.budgetai.model.BudgetLimit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface BudgetLimitRepository extends JpaRepository<BudgetLimit, Long> {
+    Optional<BudgetLimit> findByCategoryIgnoreCase(String category);
+}
